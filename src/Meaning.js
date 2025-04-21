60d84b26ffddd8ab83c3a04ef74d09367d31be49
Tracy Njoroge <tracywangari997@gmail.com>
2025-04-21 17:05:46 +0300

    Add a synonym component that renders the synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
     console.log(props.meaning);
@@ -8,7 +9,7 @@ export default function Meaning(props) {
             <h4>{props.meaning.partOfSpeech}</h4>
             <p>{props.meaning.definition}</p>
             <p><em>{props.meaning.example}</em></p>
-
+            <Synonyms synonyms={props.meaning.synonyms}/>
         </div>
     )
 };
